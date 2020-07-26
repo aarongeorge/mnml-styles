6c11ba0ba7780f177e4e98c93c1301f3bfa17e17
Aaron George <aaron.dean.george@gmail.com>
2020-07-26 15:50:17 +1000

    debugging

--- mod.ts
+++ mod.ts
@@ -9,6 +9,7 @@ console.log(3)
 if (!await exists(CONFIG_PATH)) console.error(new Error(`Could not find \`${CONFIG_NAME}\``))
 else {
 	console.log(4)
+	console.log(Deno.cwd())
 	const {default: config} = await import(`${Deno.cwd()}/${CONFIG_PATH}`)
 	console.log(5)
 	await Deno.writeTextFile(`${Deno.cwd()}/${config.outputDir}${OUTPUT_NAME}`, config.theme)
