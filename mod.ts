8012d763bc1dbd2494f7b740e67ae62b50b719c8
Aaron George <aaron.dean.george@gmail.com>
2020-07-26 19:36:28 +1000

    Initial parser support testing

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import {exists} from 'https://deno.land/std/fs/mod.ts'
+import defaultParser from './parsers/default/index.ts'
 
 const CONFIG_NAME = 'mnml-styles.config.ts'
 const CONFIG_PATH = `./${CONFIG_NAME}`
@@ -6,6 +7,9 @@ const OUTPUT_NAME = 'mnml-styles.css'
 
 if (!await exists(CONFIG_PATH)) console.error(new Error(`Could not find \`${CONFIG_NAME}\``))
 else {
+	let mnmlstylcss = ''
 	const {default: config} = await import(`file://${Deno.cwd()}/${CONFIG_PATH}`)
-	await Deno.writeTextFile(`${Deno.cwd()}/${config.outputDir}${OUTPUT_NAME}`, config.theme)
+	config.parser = config.parser || defaultParser
+	mnmlstylcss = await defaultParser.reader(config.theme).then(defaultParser.writer)
+	await Deno.writeTextFile(`${Deno.cwd()}/${config.outputDir}${OUTPUT_NAME}`, mnmlstylcss)
 }
---
+++ parsers/default/index.ts
@@ -0,0 +1,31 @@
+interface Theme {
+	properties: {
+		[property: string]: {
+			[value: string]: string
+		}
+	}
+}
+
+interface PropertySchema {
+	property: string
+	selector: string
+	value: string
+}
+
+export default {
+	reader: (theme: Theme) => {
+		let properties: Array<PropertySchema> = []
+
+		Object.entries(theme.properties).forEach(a => {
+			const [property] = a
+			Object.entries(a[1]).forEach(b => {
+				const [value, selector] = b
+				properties.push({property, selector, value})
+			})
+		})
+
+		return Promise.resolve(properties)
+	},
+
+	writer: (schemaArray: Array<PropertySchema>) => Promise.resolve(schemaArray.map(({selector, value, property}) => `${selector}{${property}:${value}}`).join('\n'))
+}
