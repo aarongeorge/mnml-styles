bb6d67eab4be2dc370f2972e57a63bf760927f51
Aaron George <aaron.dean.george@gmail.com>
2020-07-26 15:48:38 +1000

    debugging

--- mod.ts
+++ mod.ts
@@ -1,10 +1,16 @@
 import {exists} from 'https://deno.land/std/fs/mod.ts'
 
 const CONFIG_NAME = 'mnml-styles.config.ts'
+console.log(1)
 const CONFIG_PATH = `./${CONFIG_NAME}`
+console.log(2)
 const OUTPUT_NAME = 'mnml-styles.css'
+console.log(3)
 if (!await exists(CONFIG_PATH)) console.error(new Error(`Could not find \`${CONFIG_NAME}\``))
 else {
+	console.log(4)
 	const {default: config} = await import(`${Deno.cwd()}/${CONFIG_PATH}`)
+	console.log(5)
 	await Deno.writeTextFile(`${Deno.cwd()}/${config.outputDir}${OUTPUT_NAME}`, config.theme)
+	console.log(6)
 }
