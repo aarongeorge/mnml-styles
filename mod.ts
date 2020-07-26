ef76e62b457c63bd25adbb6696047c3dbde8d47a
Aaron George <aaron.dean.george@gmail.com>
2020-07-26 16:04:43 +1000

    Squashed commit of the following:
    commit 5505da7884342799761b08b2aa689905b665770a
    Author: Aaron George <aaron.dean.george@gmail.com>
    Date:   Sun Jul 26 16:04:16 2020 +1000
    
        FIXED: Not working when hosted
    
    commit 573794d12aee622f0b351a63929f18d1e54057fa
    Author: Aaron George <aaron.dean.george@gmail.com>
    Date:   Sun Jul 26 16:02:06 2020 +1000
    
        debugging
    
    commit 6c11ba0ba7780f177e4e98c93c1301f3bfa17e17
    Author: Aaron George <aaron.dean.george@gmail.com>
    Date:   Sun Jul 26 15:50:17 2020 +1000
    
        debugging
    
    commit bb6d67eab4be2dc370f2972e57a63bf760927f51
    Author: Aaron George <aaron.dean.george@gmail.com>
    Date:   Sun Jul 26 15:48:38 2020 +1000
    
        debugging

--- mod.ts
+++ mod.ts
@@ -3,8 +3,9 @@ import {exists} from 'https://deno.land/std/fs/mod.ts'
 const CONFIG_NAME = 'mnml-styles.config.ts'
 const CONFIG_PATH = `./${CONFIG_NAME}`
 const OUTPUT_NAME = 'mnml-styles.css'
+
 if (!await exists(CONFIG_PATH)) console.error(new Error(`Could not find \`${CONFIG_NAME}\``))
 else {
-	const {default: config} = await import(`${Deno.cwd()}/${CONFIG_PATH}`)
+	const {default: config} = await import(`file://${Deno.cwd()}/${CONFIG_PATH}`)
 	await Deno.writeTextFile(`${Deno.cwd()}/${config.outputDir}${OUTPUT_NAME}`, config.theme)
 }
